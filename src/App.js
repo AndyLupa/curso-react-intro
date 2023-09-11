b4cafb12a9d9aea2168e35ba9b5ef3f00197aa48
AndyLupa <alp1002@yahoo.com.ar>
2023-09-11 16:27:04 -0300

    cambie apps.js clg

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const defaultToDos = [
 
 
 function App() {
-console.log("first")
+console.log("second")
   const[searchValue, setSearchValue]=React.useState('');
   const[todo,setTodo]=React.useState(defaultToDos);
 
