04304a09e0a6c44c89379dc76d3e6475d38f514d
AndyLupa <alp1002@yahoo.com.ar>
2023-09-11 16:18:38 -0300

    cambie nada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './platzi.webp';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoItem } from './TodoItem';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoButtonCreate } from './TodoButtonCreate';
+import { TodoClick } from './TodoClick';
+
+const defaultToDos = [
+  {item : "Cortar Cebolla" , completed:true},
+  {item : 'Cortar Morron' , completed:false},
+  {item : 'Cortar Verdeo' , completed:true},
+  {item : 'Cortar Papa' , completed:true},
+];
+
+
+
 
 function App() {
+
+  const[searchValue, setSearchValue]=React.useState('');
+  const[todo,setTodo]=React.useState(defaultToDos);
+
+  const todoCompleted = todo.filter(() => !!todo.completed).length; 
+  const todoTotal=todo.length;
+  const searchedTodo = todo.filter(
+    (todo) => {
+        const todoText = todo.item.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+        return todoText.includes(searchText);
+        
+      }
+  );
+
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter completed={todoCompleted} total={todoTotal}/>
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      <TodoList>
+        {searchedTodo.map(todo=>(
+          <TodoItem 
+            key={todo.item} 
+            item={todo.item}
+            completed={todo.completed} 
+          />
+        ))}
+      </TodoList>
+      <TodoButtonCreate/>
+      <TodoClick></TodoClick>
+
+          </>
   );
 }
 
